bc2d1133ee02d0f455f41e0724d0fe48e1b1ea04
GarettMorrison <54683354+GarettMorrison@users.noreply.github.com>
2020-08-05 20:43:14 -0500

    Update Pastebin_Installer.lua

--- simple/Pastebin_Installer.lua
+++ simple/Pastebin_Installer.lua
@@ -9,3 +9,8 @@ local sand = fs.open("sand", "w")
 str = http.get("https://raw.githubusercontent.com/GarettMorrison/ComputerCraft/master/simple/sand.lua").readAll()
 sand.write(str)
 sand.close()
+
+local fuel = fs.open("fuel", "w")
+str = http.get("https://raw.githubusercontent.com/GarettMorrison/ComputerCraft/master/simple/refuel.lua").readAll()
+fuel.write(str)
+fuel.close()
