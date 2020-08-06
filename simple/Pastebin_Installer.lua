e9f593b3183a7f8c4e838fd795fc37d01a1b2ce6
GarettMorrison <54683354+GarettMorrison@users.noreply.github.com>
2020-08-05 20:42:02 -0500

    Create Pastebin_Installer.lua

---
+++ simple/Pastebin_Installer.lua
@@ -0,0 +1,11 @@
+--Install using pastebin run
+
+local quarry = fs.open("quarry", "w")
+local str = http.get("https://raw.githubusercontent.com/GarettMorrison/ComputerCraft/master/simple/quarry.lua").readAll()
+quarry.write(str)
+quarry.close()
+
+local sand = fs.open("sand", "w")
+str = http.get("https://raw.githubusercontent.com/GarettMorrison/ComputerCraft/master/simple/sand.lua").readAll()
+sand.write(str)
+sand.close()
